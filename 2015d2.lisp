98d87db075624c3f6209f557cc36d5af709ee09a
Hokanson <Kyle.Hokanson@emerson.com>
2026-05-18 16:12:14 -0500

    2015 d2 p2 works!

--- 2015d2.lisp
+++ 2015d2.lisp
@@ -10,17 +10,28 @@
                         :length 2)
     (+ (apply #'min areas) (* 2 (reduce #'+ areas)))))
 
-(let* ((day-number 2)
-       (input-name (format nil "~dd~dinput.txt" *year* day-number)))
-  (defday 2
-    :test-input
-    '(("2x3x4"  58)
-      ("1x1x10" 43))
-    :parse ((mapcar (lambda (line)
-                      (mapcar #'parse-integer (str:split #\x line)))
-                    input))
-    :p1 ((reduce #'+ (mapcar #'d2-needed-area (day-2-parse input))))
-    :p1-test ((destructuring-bind (lines expected)
-                  (apply #'mapcar #'list input)
-                (every #'= (mapcar #'d2-needed-area (day-2-parse lines))
-                       expected)))))
+(defun d2-needed-ribbon (dimension-list)
+  (let* ((smallest-face (subseq (sort (copy-list dimension-list) #'<) 0 2))
+         (volume (apply #'* dimension-list)))
+    (+ volume (* 2 (apply #'+ smallest-face)))))
+
+(defday 2
+  :test-input
+  '(("2x3x4"  58 34)
+    ("1x1x10" 43 14))
+  :parse ((mapcar (lambda (line)
+                    (mapcar #'parse-integer (str:split #\x line)))
+                  input))
+  :p1 ((reduce #'+ (mapcar #'d2-needed-area (day-2-parse input))))
+  :p1-test ((destructuring-bind (lines expected ign)
+                (apply #'mapcar #'list input)
+              (declare (ignore ign))
+              (every #'= (mapcar #'d2-needed-area (day-2-parse lines))
+                     expected)))
+  :p2 ((reduce #'+ (mapcar #'d2-needed-ribbon (day-2-parse input))))
+  :p2-test ((destructuring-bind (lines ign expected)
+                (apply #'mapcar #'list input)
+              (declare (ignore ign))
+              (every #'= (mapcar #'d2-needed-ribbon (day-2-parse lines))
+                     expected))))
+
