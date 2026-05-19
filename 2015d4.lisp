fb85d3bbb691c8206ca38764696a9fa84d34a105
Hokanson <Kyle.Hokanson@emerson.com>
2026-05-19 11:53:15 -0500

    2015 d5 p1 and p2 work

---
+++ 2015d4.lisp
@@ -0,0 +1,27 @@
+;;;2015 day 4
+
+(in-package :aoc-2015)
+
+(require 'sb-md5) ;;no, i'm not implementing this myself
+
+(defparameter *day-4-input* "yzbqklnj")
+
+(defun md5vec->str (md5vec)
+  (format nil "~{~2,'0x~}" (coerce (subseq md5vec 0 4) 'list)))
+
+(defday 4
+  :test-input '("abcdef"
+                "pqrstuv")
+  :p1-test-expected '(609043
+                      1048970)
+  :parse ()
+  :p1 ((loop :for ans :from 1
+             :when (string= "00000"
+                            (md5vec->str (sb-md5:md5sum-string (format nil "~a~a" input ans)))
+                            :end2 5)
+               :return ans))
+  :p2 ((loop :for ans :from 1
+             :when (equalp #(0 0 0)
+                          (subseq (sb-md5:md5sum-string (format nil "~a~a" input ans))
+                                  0 3))
+               :return ans)))
