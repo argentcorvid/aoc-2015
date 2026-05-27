6098ad5333cbe7cb411317470dff6257fdd06a38
Hokanson <Kyle.Hokanson@emerson.com>
2026-05-27 15:02:48 -0500

    2015 d8 p1 correct at 1342, needed to nest negative lookbehinds for hex match

--- 2015d8.lisp
+++ 2015d8.lisp
@@ -2,30 +2,6 @@
 
 (in-package :aoc-2015)
 
-(defparameter *d8-escaped-hex* (ppcre:create-scanner "(?<!\\\\){1}\\\\x([0-9a-f]{2})"))
-
-(defun unescape-hex-match (match number)
-  (declare (ignore match))
-  (format nil "~a" (code-char (parse-integer number :radix 16))))
-
-(defun count-chars (str)
-  (if (find #\\ str)
-      (let ((str (copy-seq str)))
-       (ppcre:regex-replace-all *d8-escaped-hex*
-                                str
-                                #'unescape-hex-match
-                                :simple-calls t)
-       (ppcre:regex-replace-all (ppcre:quote-meta-chars "\\\\") 
-                               str
-                               (ppcre:quote-meta-chars "\\"))
-       (ppcre:regex-replace-all (ppcre:quote-meta-chars "\\\"")
-                                str
-                                (ppcre:quote-meta-chars "\""))
-      )
-      (cons (length str) (- (length str) 2))))
-
-
-
 (defday 8
   :test-input '("\"\""
                 "\"abc\""
@@ -33,7 +9,7 @@
                 "\"\\x27\"")
   :parse ()
   :p1 ((let ((single-escape (ppcre:create-scanner "\\\\\\\\|\\\\\\\""))
-             (hex-escape (ppcre:create-scanner "(?<!\\\\)\\\\x[0-9a-z]{2}"))
+             (hex-escape (ppcre:create-scanner "(?<!(?<!\\\\)\\\\)\\\\x[0-9a-f]{2}"))
              (code-length 0)
              (string-length 0))
          (dolist (line input) ;;; change to reduce
