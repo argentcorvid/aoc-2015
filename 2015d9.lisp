bb9d27df6b7f5c3c78c7a48b06d87b9b3c85c178
Hokanson <Kyle.Hokanson@emerson.com>
2026-08-19 14:33:14 -0500

    day 9 p1 and p2 works.
    used permutation method from reddit thread

--- 2015d9.lisp
+++ 2015d9.lisp
@@ -2,36 +2,46 @@
 
 (in-package :aoc-2015)
 
+(defun pick-elements (in-seq &rest indices)
+  (let ((out (list)))
+    (dotimes (i (length indices))
+      (push (elt in-seq (elt indices i)) out))
+    (nreverse out)))
+
+(defun day-9-distances (city-list pair-lookup)
+  (let ((all-route-distances (list)))
+    (a:map-permutations
+     (lambda (route-order)
+       (let ((a (butlast route-order))
+             (b (rest route-order)))
+         (push
+          (reduce #'+ (mapcar (lambda (city-a city-b)
+                                (gethash (list city-a city-b) pair-lookup))
+                              a b))
+          all-route-distances)))
+     city-list)
+    all-route-distances))
+
 (defday 9
   :test-input '("London to Dublin = 464"
                 "London to Belfast = 518"
                 "Dublin to Belfast = 141")
-  :parse ((let ((map (make-hash-table :test 'equal)))
-            (dolist (line input)
-              (let ((parts (delete-if (lambda (w) (or (string= w "to")
-                                                      (string= w "=")))
-                                      (str:words line))))
-                (setf (third parts) (parse-integer (third parts)))
-                (push (subseq parts 1 3) (gethash (first parts) map))))
-            map))
-  :p1 ((loop :for k :being :each :hash-key :of input
-             :do (vformat "~&*new start point ~a" k)
-             :minimize (bfs-distance k input)))
-  :p2 ())
-
-(defun bfs-distance (start graph) ;;this is doing directed graph, need undirected!
-  (loop :with visited := (list)
-        :and queue := (list start)
-        :and distance := 0
-        :until (null queue)
-        :for current := (pop queue)
-        :do (vformat "~&visiting ~a" current)
-            (push current visited)
-            (loop :for (dest dist) :in (gethash current graph)
-                  :unless (find dest visited :test #'string=)
-                    :do (vformat "~& destination ~a not yet visited, adding to queue" dest)
-                       (a:appendf queue (list dest))
-                  :do (vformat "~& distance to destination ~a: ~d, adding to total" dest dist)
-                      (incf distance dist)
-                      (vformat "~& total now: ~d" distance))
-        :finally (return distance)))
+  
+  :parse ((loop :with all-cities := (list)
+                :and distances := (make-hash-table :test 'equal)
+                :for line :in input
+                :for parts := (str:words line)
+                :for route-cities := (pick-elements parts 0 2)
+                :do (a:unionf all-cities route-cities :test #'string=)
+                    (let ((dist (parse-integer (nth 4 parts))))
+                      (setf (gethash route-cities distances)
+                            dist
+                            (gethash (reverse route-cities) distances)
+                            dist))
+                :finally (return (values all-cities distances))))
+  :p1 ((princ (reduce #'min (day-9-distances input (first more)))))
+  :p1-test ((multiple-value-call #'day-9-p1 (day-9-parse %test-input%)))
+  :p1-test-expected 605
+  :p2 ((princ (reduce #'max (day-9-distances input (first more)))))
+  :p2-test ((multiple-value-call #'day-9-p2 (day-9-parse %test-input%)))
+  :p2-test-expected 982)
