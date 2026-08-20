c4b5aecd3b071d307a7fad3fa6301e3df6913c66
Hokanson <Kyle.Hokanson@emerson.com>
2026-08-20 16:17:37 -0500

    d11 p1 increment-password mostly working, need to fix carrying

---
+++ 2015d11.lisp
@@ -0,0 +1,63 @@
+;;;2015 day 11
+
+(in-package :aoc-2015)
+
+(defparameter day-11-input "hepxcrrq")
+
+(defun iol-p (char-in)
+  (case char-in ((#\i #\o #\l) t)))
+
+(defun no-iol-p (string-in)
+  (notany #'iol-p string-in))
+
+(defun straight-of-three-p (string-in)
+  (search '(1 1) (s:deltas string-in (lambda (a b) (- (char-code a) (char-code b)))))
+  ;; (loop :with alpha := "abcdefghijklmnopqrstuvwxyz"
+  ;;       :for start :from 0
+  ;;       :and end :from 3 :to (length alpha)
+  ;;              :thereis (search alpha string-in :start1 start :end1 end)
+  ;;       )
+  )
+
+(defun two-pairs-p (string-in)
+  (<= 2
+      (count-if
+       (lambda (run)
+         (>= (length run) 2))
+       (s:runs string-in))))
+
+(defun valid-password-p (string-in)
+  (and (no-iol-p string-in)
+       (straight-of-three-p string-in)
+       (two-pairs-p string-in)))
+
+(defun char-to-num (char-in)
+  (- (char-code char-in) 97))
+
+(defun num-to-char (int-in)
+  (code-char (+ int-in 97)))
+
+(defun next-char (char &optional (carry 0))
+  (multiple-value-bind
+        (carry out)
+      (floor (+ (char-to-num char) 1 carry) 26)
+    (values (num-to-char out) carry)))
+
+(defun increment-password (string-in)
+  (let ((new-pwd
+          (loop :with carry-in := 0
+                :for c :across (reverse string-in)
+                :for (new carry-out) := (multiple-value-list (next-char c carry-in))
+                :collect new :into out
+                :do (setf carry-in carry-out)
+                :until (zerop carry-in)
+                :finally (return (replace string-in (reverse out) :start1 (- (length string-in) (length out)))))))
+    (a:if-let ((p (position-if #'iol-p new-pwd)))
+      (increment-password (fill new-pwd #\z :start (1+ p)))
+      new-pwd)))
+
+(defday 11
+  :test-input ""
+  :parse ()
+  :p1 ()
+  :p2 ())
