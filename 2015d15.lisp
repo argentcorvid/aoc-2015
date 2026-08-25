141fa0a12439bbff3d60f23b4464700e2e2b8f9d
Hokanson <Kyle.Hokanson@emerson.com>
2026-08-25 10:34:56 -0500

    d15p1 works
    - used 'partitions' from reddit thread https://www.reddit.com/r/adventofcode/comments/3wwj84/day_15_solutions/

--- 2015d15.lisp
+++ 2015d15.lisp
@@ -2,20 +2,32 @@
 
 (in-package :aoc-2015)
 
-(defun score-cookie (ingredient-amounts ingredient-stats &optional (part-1 t))
-  (loop :with stats := (if part-1
-                           (mapcar (a:rcurry #'s:slice 0 -1) ingredient-stats)
-                           ingredient-stats)
-        :for (ing amt) :in ingredient-amounts
+(defun score-cookie (ingredient-amounts ingredient-stats)
+  (loop :for (ing amt) :in ingredient-amounts
         :collect (mapcar (lambda (stat)
                            (* amt stat))
                          (s:assocdr ing
-                                    stats
+                                    ingredient-stats
                                     :test #'string=))
           :into score-components
         :finally (return (reduce #'*
                                  (apply #'mapcar #'+ score-components)
-                                 :key (lambda (s) (if (minusp s) 0 s))))))
+                                 :key (lambda (s) (if (not (minusp s)) s 0))))))
+
+(s:-> partition (fixnum fixnum) list)
+(let ((cache (make-hash-table :test 'equal)))
+  (defun partition (total ways)
+    (multiple-value-bind (val present)
+        (gethash (list total ways) cache)
+      (if present
+          val
+          (setf (gethash (list total ways) cache)
+                (cond ((zerop ways) nil)
+                      ((= 1 ways)
+                       (list (list total)))
+                      (t (loop :for h fixnum :in (a:iota (1+ total))
+                               :nconc (loop :for tt :in (partition (- total h) (1- ways))
+                                            :collect (append (list h) tt))))))))))
 
 (defday 15
   :test-input
@@ -30,7 +42,13 @@ Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3")
                                    (mapcar (a:rcurry #'parse-integer :junk-allowed t)
                                            (pick-elements stats 1 3 5 7 9))
                                    table))))))
-  :p1 ((let ((ingredient-budget 100)
-             (ingredient-names (mapcar #'first input)))
-         ))
+  :p1 ((let* ((ingredient-budget 100)
+              (ingredient-names (mapcar #'first input))
+              (stats  (mapcar (a:rcurry #'s:slice 0 -1) input)) ;; "ignore calories *for now*"
+              )
+         (loop :for amounts :in (partition ingredient-budget (length ingredient-names))
+               :maximize (score-cookie (mapcar #'list ingredient-names amounts) stats))))
+  :p1-test-expected 62842880
+  :p1-test ((let ((v (day-15-p1 (day-15-parse %test-input%))))
+              (values v (= %p1-expect% v))) )
   :p2 ())
