833b2a4972c5ed4730b15bcd018641353955c2f3
Hokanson <Kyle.Hokanson@emerson.com>
2026-08-26 14:21:16 -0500

    d16p2 list structure fixed - p1 and p2 runs

--- 2015d16.lisp
+++ 2015d16.lisp
@@ -90,19 +90,20 @@ perfumes: 1")))
   (defun day-16-p1-bf (input)
     (1+ (position-if (a:rcurry #'subsetp keys :test #'string=) input)))
   (defun day-16-p2-bf (input)
-    (1+ (position-if (lambda (sue-line)               ;(a:rcurry #'subsetp keys :test #'string=)
-                       (subsetp sue-line keys
-                                :test (lambda (line-itm key-itm)
-                                        (match key-itm
-                                          ((ppcre "(cats|trees): (\\d+)"
-                                                  kind number)
-                                           (and (string= line-itm kind :end1 (1+ (position #\: line-itm)))
-                                                (< (parse-integer number)
-                                                   (parse-integer line-itm :start (position #\space line-itm) :junk-allowed t))))
-                                          ((ppcre "(pomeranians|goldfish): (\\d+)"
-                                                  kind number)
-                                           (and (string= line-itm kind :end1 (1+ (position #\: line-itm)))
-                                                (> (parse-integer number)
-                                                   (parse-integer line-itm :start (position #\space line-itm) :junk-allowed t))))
-                                          (otherwise (string= line-itm key-itm))))))
-                     input))))
+    (1+ (position-if
+         (lambda (sue-line)          ;(a:rcurry #'subsetp keys :test #'string=)
+           (subsetp sue-line keys
+                    :test (lambda (line-itm key-itm)
+                            (match key-itm
+                              ((ppcre "(cats|trees): (\\d+)"
+                                      kind number)
+                               (and (string= line-itm kind :end1 (position #\: line-itm))
+                                    (< (parse-integer number)
+                                       (parse-integer line-itm :start (position #\space line-itm) :junk-allowed t))))
+                              ((ppcre "(pomeranians|goldfish): (\\d+)"
+                                      kind number)
+                               (and (string= line-itm kind :end1 (position #\: line-itm))
+                                    (> (parse-integer number)
+                                       (parse-integer line-itm :start (position #\space line-itm) :junk-allowed t))))
+                              (otherwise (string= line-itm key-itm))))))
+         input))))
