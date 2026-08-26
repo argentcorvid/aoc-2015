246bde7609acf8ad5376842cae6c97a552dae3a0
Hokanson <Kyle.Hokanson@emerson.com>
2026-08-26 16:33:03 -0500

    d17p1 and p2 work!

---
+++ 2015d17.lisp
@@ -0,0 +1,15 @@
+;;;2015 day 17
+
+(in-package :aoc-2015)
+
+(defday 17
+  :p1 ((count-if (lambda (containers)
+                   (= 150 (reduce #'+ (mapcar #'parse-integer containers))))
+                 (rest (s:powerset (uiop:read-file-lines *day17input*)))))
+  :p2 ((let* ((valid-containers
+               (remove-if-not
+                (lambda (containers)
+                  (= 150 (reduce #'+ (mapcar #'parse-integer containers))))
+                (rest (s:powerset (uiop:read-file-lines *day17input*)))))
+              (minimum-containers (apply #'min (mapcar #'length valid-containers))))
+         (count-if (lambda (itm) (= (length itm) minimum-containers)) valid-containers))))
