a2d4732a31338f85281e9dbe8b958ff9d3ab5e75
Hokanson <Kyle.Hokanson@emerson.com>
2026-08-27 08:04:56 -0500

    d17 apply instead of reduce

--- 2015d17.lisp
+++ 2015d17.lisp
@@ -4,12 +4,12 @@
 
 (defday 17
   :p1 ((count-if (lambda (containers)
-                   (= 150 (reduce #'+ (mapcar #'parse-integer containers))))
+                   (= 150 (apply #'+ (mapcar #'parse-integer containers))))
                  (rest (s:powerset (uiop:read-file-lines *day17input*)))))
   :p2 ((let* ((valid-containers
                (remove-if-not
                 (lambda (containers)
-                  (= 150 (reduce #'+ (mapcar #'parse-integer containers))))
+                  (= 150 (apply #'+ (mapcar #'parse-integer containers))))
                 (rest (s:powerset (uiop:read-file-lines *day17input*)))))
               (minimum-containers (apply #'min (mapcar #'length valid-containers))))
          (count-if (lambda (itm) (= (length itm) minimum-containers)) valid-containers))))
