d8e4554cb9d27050d313253255d027702991d9ab
Kyle Hokanson <khokanson@gmail.com>
2026-08-28 06:50:05 -0500

    d19 finish parsing - work on getting next molecule

--- 2015d19.lisp
+++ 2015d19.lisp
@@ -1,6 +1,13 @@
 ;;;2015 day 19
+;;;https://www.reddit.com/r/adventofcode/comments/3xflz8/day_19_solutions/cy4cq4j/
+
 
 (in-package :aoc-2015)
+
+(defun next-molecule (molecule r-map)
+  (loop :for ch :across molecule
+        :and i :from 0
+        :for r := (gethash ch r-map)))
 
 (defday 19
   :test-input
@@ -16,6 +23,6 @@ HOH
               (dolist (line (str:lines reps))
                 (apply (a:rcurry #'s:addhash r-map)
                        (str:split " => " line :regex t)))
-              )))
+              (values r-map molecule))))
   :p1 ()
   :p2 ())
