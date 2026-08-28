59fe288c873b77a02fad14d4e12ce2719cd0c86d
Hokanson <Kyle.Hokanson@emerson.com>
2026-08-28 11:03:42 -0500

    d19 runs. test ok, full input too high at 603

--- 2015d19.lisp
+++ 2015d19.lisp
@@ -4,10 +4,23 @@
 
 (in-package :aoc-2015)
 
+(S:-> next-molecule (string hash-table) hash-table)
 (defun next-molecule (molecule r-map)
-  (loop :for ch :across molecule
+  (loop :with new-molecules := (s:dict)
+        :for ch :across molecule
         :and i :from 0
-        :for r := (gethash ch r-map)))
+        :do (loop :for r :in (or (gethash (string ch) r-map)
+                                 (gethash (s:slice molecule i (+ 2 i)) r-map))
+                  :when r
+                    :do (setf (gethash (str:concat (s:slice molecule 0 i)
+                                                   r
+                                                   (s:slice molecule (1+ i )))
+                                       new-molecules)
+                              t))            
+        :finally (return new-molecules)))
+
+(defun count-molecules (molecule r-map)
+  (hash-table-count (next-molecule molecule r-map)))
 
 (defday 19
   :test-input
@@ -23,6 +36,9 @@ HOH
               (dolist (line (str:lines reps))
                 (apply (a:rcurry #'s:addhash r-map)
                        (str:split " => " line :regex t)))
-              (values r-map molecule))))
-  :p1 ()
+              (values molecule r-map))))
+  :p1 ((multiple-value-call #'count-molecules (day-19-parse input)))
+  :p1-test ((let ((r (day-19-p1 %test-input%)))
+              (values r (= r %p1-expect%))))
+  :p1-test-expected 4
   :p2 ())
