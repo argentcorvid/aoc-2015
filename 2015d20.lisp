220b14162d90b9acecacbe2012f4692418d2efcb
ArgentCorvid <khokanson@gmail.com>
2026-09-01 06:58:27 -0500

    d20p1 sum and collect wrong

--- 2015d20.lisp
+++ 2015d20.lisp
@@ -16,34 +16,49 @@
 ;; new target = target - 1
 ;; new target = 3_399_999
 ;; new target is divisible by 3 -> house > 3!
-;; target3 = 1_133_333 = (hn + oe + 1)/3
+;; target3 = 1_133_333 = (hn + oe)/3
+;; cant just do primes - 20 and 100 are missing 10!
 
-(defparameter *day20primes* (list 2      3      5      7     11     13     17     19     23     29 
-     31     37     41     43     47     53     59     61     67     71 
-     73     79     83     89     97    101    103    107    109    113 
-    127    131    137    139    149    151    157    163    167    173 
-    179    181    191    193    197    199    211    223    227    229 
-    233    239    241    251    257    263    269    271    277    281 
-    283    293    307    311    313    317    331    337    347    349 
-    353    359    367    373    379    383    389    397    401    409 
-    419    421    431    433    439    443    449    457    461    463 
-    467    479    487    491    499    503    509    521    523    541 
-    547    557    563    569    571    577    587    593    599    601 
-    607    613    617    619    631    641    643    647    653    659 
-    661    673    677    683    691    701    709    719    727    733 
-    739    743    751    757    761    769    773    787    797    809 
-    811    821    823    827    829    839    853    857    859    863 
-    877    881    883    887    907    911    919    929    937    941 
-    947    953    967    971    977    983    991    997   1009)) ;;not going to be enough - read in from file?
+
+(defparameter *day20primes*
+  (list 2      3      5      7     11     13     17     19     23     29 
+        31     37     41     43     47     53     59     61     67     71 
+        73     79     83     89     97    101    103    107    109    113 
+        127    131    137    139    149    151    157    163    167    173 
+        179    181    191    193    197    199    211    223    227    229 
+        233    239    241    251    257    263    269    271    277    281 
+        283    293    307    311    313    317    331    337    347    349 
+        353    359    367    373    379    383    389    397    401    409 
+        419    421    431    433    439    443    449    457    461    463 
+        467    479    487    491    499    503    509    521    523    541 
+        547    557    563    569    571    577    587    593    599    601 
+        607    613    617    619    631    641    643    647    653    659 
+        661    673    677    683    691    701    709    719    727    733 
+        739    743    751    757    761    769    773    787    797    809 
+        811    821    823    827    829    839    853    857    859    863 
+        877    881    883    887    907    911    919    929    937    941 
+        947    953    967    971    977    983    991    997   1009)) ;;not going to be enough - read in from file?
 
 (defun collect-divisors (number)
   (let (divisors)
-    (dolist (d (remove-if (a:curry #'<= number) *day20primes*))
+    (dolist (d *day20primes*)
+      (when (>= d number)
+        (return (nconc (list 1 number) divisors)))
       (multiple-value-bind (q r)
           (floor number d)
         (when (zerop r)
-          (push (list q d) divisors))))
-    (sort (nconc (list 1 number) divisors) #'<))) ;don't need to sort here, will be to slow, do it when printing
+          (a:nconcf divisors (list q d)))))))
+
+(defun sum-divisors (number)
+  (let ((sum 0))
+    (dolist (d *day20primes*)
+      (declare (fixnum sum number d))
+      (when (>= d number)
+        (return (+ 1 number sum)))
+      (multiple-value-bind (q r)
+          (floor number d)
+        (when (zerop r)
+          (incf sum (+ d q)))))))
 
 (defday 20
   :test-input ""
